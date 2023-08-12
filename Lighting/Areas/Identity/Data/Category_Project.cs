803629b7db49627343dfb96b0dda39cee790d2ed
Kittipong_F <kittipongtoy@hotmail.com>
2023-08-12 13:17:57 +0700

    ....

--- Lighting/Areas/Identity/Data/Category_Project.cs
+++ Lighting/Areas/Identity/Data/Category_Project.cs
@@ -3,9 +3,9 @@
     public class Category_Project
     {
         public int Id { get; set; }
-        public string Name_TH { get; set; }
-        public string Name_EN { get; set; }
-        public string Image_Path { get; set; }
-        public string Image_Path_Nav { get; set; }
+        public string? Name_TH { get; set; }
+        public string? Name_EN { get; set; }
+        public string? Image_Path { get; set; }
+        public string? Image_Path_Nav { get; set; }
     }
 }
